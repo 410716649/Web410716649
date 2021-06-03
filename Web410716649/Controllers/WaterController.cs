77dc0e7266024261e19ddfcbb146064059ac15c9
黃奕翔 <easom0817@gmail.com>
2021-06-03 19:38:45 +0800

    註冊

---
+++ Web410716649/Controllers/WaterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410716649.Controllers
+{
+    public class WaterController : Controller
+    {
+        public ActionResult Water(string name, float? weight)
+        {
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "cc";
+            }
+
+            ViewBag.name = name;
+            ViewBag.Result = result;
+
+            return View();
+        }
+    }
+}
